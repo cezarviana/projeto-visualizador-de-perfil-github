03201c07f16c0461b399a301564f61315329b659
Cezar Viana <cezar.viana@gmail.com>
2026-01-19 17:09:28 -0300

    feat: busca os dados do user na api e apresenta em tela

--- src/js/index.js
+++ src/js/index.js
@@ -2,25 +2,59 @@ const baseURL = 'https://api.github.com';
 
 const inputSearch = document.getElementById('input-search');
 const btnSearch = document.getElementById('btn-search');
+const profileResults = document.querySelector('.profile-results');
 
 btnSearch.addEventListener('click', async () => {
 
     const userName = inputSearch.value;
 
     if (userName) {
-        
-        const response = await fetch(`${baseURL}/users/${userName}`);
+        profileResults.innerHTML = `<p class="loading">Carregando...</p>`;
 
-        if (!response.ok) {
-            alert('Usuário não encontrado no GitHub.');
-            return;
+        try {
+
+            const response = await fetch(`${baseURL}/users/${userName}`);
+
+            if (!response.ok) {
+                alert('Usuário não encontrado no GitHub.');
+                profileResults.innerHTML = '';
+                return;
+            }
+
+
+
+            const userData = await response.json();
+            console.log(userData);
+            console.log(userData.avatar_url);
+            console.log(userData.name);
+            console.log(userData.bio);
+            console.log(userData.followers);
+            console.log(userData.following);
+
+            profileResults.innerHTML = `
+            <div class="profile-card">
+                <img src="${userData.avatar_url}" alt="Avatar de ${userData.name}" class="profile-avatar">
+                <div class="profile-info">
+                    <h2>${userData.name || 'Nome não disponível'}</h2>
+                    <p>${userData.bio || 'Não possui bio cadastrada 😢'}</p>
+                    <p>Seguidores: ${userData.followers}</p>
+                    <p>Seguindo: ${userData.following}</p>
+                </div>
+            </div>
+            `
+
+
+        } catch (error) {
+            console.error('Erro ao buscar o usuário:', error);
+            alert('Ocorreu um erro ao buscar o usuário. Por favor, tente novamente mais tarde.');
+            profileResults.innerHTML = '';
         }
 
-        const data = await response.json();
-        console.log(data);
 
     } else {
         alert('Por favor, digite um nome de usuário do GitHub.');
+        profileResults.innerHTML = '';
     }
 
-});
+}
+);
